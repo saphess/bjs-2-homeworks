35c0b1829007d88717f47ca6cba016f5933a0744
Maria <saphess@mail.ru>
2025-04-07 23:19:19 +0300

    Третья работа готова

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length ? arr1.every((e, i) => e === arr2[i]) : false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(user => user.gender === gender)
+    .reduce((acc, user, index, arr) => {
+      acc += user.age;
+      if (index === arr.length - 1) {
+        return acc / arr.length;
+      }
+      return acc;
+    }, 0);
 }
